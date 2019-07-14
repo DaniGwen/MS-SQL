526bd145ac2e7e9f467784878a8cda7b52eabd18
Daniel G <dani.gwenski@gmail.com>
2019-07-14 13:15:30 +0300

    Added ImportCategoriesProducts project

---
+++ Json_Processing_Exercises/ImportCategoriesProductsFromJson/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using p01_Products_Shop_Database.Data;
+using p01_Products_Shop_Database.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ImportCategoriesProductsFromJson
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ProductsShopContext())
+            {
+                var jsonFile = @"C:\categories-products.json";
+
+                var result = ImportCategoryProducts(context, jsonFile);
+
+                Console.WriteLine(result);
+            }
+
+        }
+
+        public static string ImportCategoryProducts(ProductsShopContext context, string inputJson)
+        {
+            var categoryProducts = new List<CategoryProduct>();
+
+            try
+            {
+                categoryProducts = JsonConvert.DeserializeObject<List<CategoryProduct>>(File.ReadAllText(inputJson));
+            }
+            catch (Exception e)
+            {
+
+                throw e.InnerException;
+            }
+
+            try
+            {
+                context.CategoryProducts.AddRange(categoryProducts);
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return $"Successfully imported {context.CategoryProducts.Count()}";
+        }
+    }
+}
