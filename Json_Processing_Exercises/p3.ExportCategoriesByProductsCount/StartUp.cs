a864e3dc385668bc6443ffc301101401327b8e3d
Daniel G <dani.gwenski@gmail.com>
2019-12-28 21:53:24 +0200

    update

---
+++ Json_Processing_Exercises/p3.ExportCategoriesByProductsCount/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using p01_Products_Shop_Database.Data;
+using p01_Products_Shop_Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p3.ExportCategoriesByProductsCount
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ProductsShopContext())
+            {
+                var result = GetCategoriesByProductsCount(context);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetCategoriesByProductsCount(ProductsShopContext context)
+        {
+            var products = context.Products
+                .ToList();
+
+            var categories = context.Categories
+                .Include(c => c.CategoryProducts)
+                .Where(c => c.Name != null)
+                .Select(x => new objectDto
+                {
+                    Id = x.Id,
+                    Category = x.Name,
+                    ProductsCount = x.CategoryProducts.Count(),
+                    TotalRevenue = x.CategoryProducts.Sum(c => c.Product.Price),
+                    AveragePrice = x.CategoryProducts.Average(c => c.Product.Price)
+                })
+                .OrderByDescending(x => x.ProductsCount)
+                .ToList();
+
+            var resultJson = JsonConvert.SerializeObject(categories, Formatting.Indented);
+
+            return resultJson;
+        }
+    }
+}
---
+++ Json_Processing_Exercises/p3.ExportCategoriesByProductsCount/objectDto.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace p3.ExportCategoriesByProductsCount
+{
+    public class objectDto
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "category")]
+        public string Category { get; set; }
+
+        [JsonProperty(PropertyName = "productsCount")]
+        public int ProductsCount { get; set; }
+
+        [JsonProperty(PropertyName = "averagePrice")]
+        public decimal AveragePrice { get; set; }
+
+        [JsonProperty(PropertyName = "totalRevenue")]
+        public decimal TotalRevenue { get; set; }
+    }
+}
